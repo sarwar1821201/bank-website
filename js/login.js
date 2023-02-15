9ac032ecd520e40a3bbe922f0dc29146be834e57
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-02-15 22:43:54 +0600

    add first part

---
+++ js/login.js
@@ -0,0 +1,22 @@
+//console.log('page connected');
+
+// add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //console.log('submit button clicked');
+
+    // step:2 get the email address from the email input field
+
+    // always remember to use (.value) to get text from an input field
+    const emailField= document.getElementById('user-email');
+    const email= emailField.value;
+    //console.log(email);
+
+    //step:3- get password
+    // 3.a. set id on the html element
+    // 3.b. get the element
+    // 3.c. get the value from the element
+    
+    const passwordField= document.getElementById('user-password');
+    const password= passwordField.value;
+    console.log(email, password);
+})
